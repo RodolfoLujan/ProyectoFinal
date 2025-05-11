f3b47e6b87d2db66ec7ac7b183af6d69103dc3a3
ShadowKnSs <xavirohe123@hotmail.com>
2025-05-11 11:31:21 -0600

    Detalles del Producto

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../types/product';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+  route = inject(ActivatedRoute);
+  productService = inject(ProductService);
 
+  product: Product | null = null;
+
+  ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.productService.getProductById(id).subscribe((res) => {
+      this.product = res;
+    });
+  }
+
+  getDiscountedPrice(price: number, discount: number): number {
+    return discount ? Math.round(price * (1 - discount / 100)) : price;
+  }
 }
